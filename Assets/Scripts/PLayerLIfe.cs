b02726c8f4c296461ff2f5f29027fc959113a263
Emre50 <devecie03@gmail.com>
2024-01-07 15:52:26 +0300

    Project Files

---
+++ Assets/Scripts/PLayerLIfe.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PLayerLIfe : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    [SerializeField] private AudioSource deathSound;
+
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+            deathSound.Play();
+
+        }
+    }
+
+    private void Die()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        anim.SetTrigger("death");
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
